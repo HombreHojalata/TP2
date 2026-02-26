2eae83f746f3f100f993697a54ae0a908b26e1e8
Manuel del Caño <146079243+HombreHojalata@users.noreply.github.com>
2026-02-26 13:01:28 +0100

    Delete TP1 directory

--- TP1/src/simulator/model/AnimalMapView.java
+++
@@ -1,7 +0,0 @@
-package simulator.model;
-
-import java.util.List;
-import java.util.function.Predicate;
-public interface AnimalMapView extends MapInfo, FoodSupplier {  
-	public List<Animal> getAnimalsInRange(Animal a, Predicate<Animal> pAnimal);
-}
